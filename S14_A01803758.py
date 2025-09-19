77be548a1fbbb1867463c0c75425a4efecb3c12f
emileo039-droid <emileo039@gmail.com>
2025-09-19 08:26:31 -0600

    S14_A01803758.py

---
+++ S14_A01803758.py
@@ -0,0 +1,17 @@
+#ejercicio 3
+fechaNacimiento = int(input("Ingresa el año en que naciste: "))
+anioDeHoy = 2025
+edad = anioDeHoy - fechaNacimiento
+print("Tienes", edad, "años")
+#ejercicio 2
+edad = int(input("Ingresa tu edad: "))
+if 13 <= edad <= 19:
+    print("Eres un chamaco")
+else:
+    print("No eres un chamaco")
+  #ejercicio 1
+  edad = int(input("Ingresa tu edad: "))
+if edad >= 18:
+    print("ruco")
+else:
+    print("muy bien")
